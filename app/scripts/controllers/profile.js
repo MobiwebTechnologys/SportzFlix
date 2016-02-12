7604c7df5e4007d88e50f6fc27eebddb683dd45d
satishmobiweb <satish@mobiwebtech.com>
2016-02-12 12:22:44 +0530

    solve profile error

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -8,170 +8,169 @@
  * Controller of the sportzflixApp
  */
 angular.module('sportzflixApp')
-  .controller('ProfileCtrl', function ($scope, profilemanagement, $modal, $window, auth) {
+    .controller('ProfileCtrl', function ($scope, profilemanagement, $uibModal, $window, auth) {
 
-    $("body").css('background', 'none');
-      $("body").css('background-color', '#212121');
-
-
-    $scope.auth = auth;
-
+        $("body").css('background', 'none');
+        $("body").css('background-color', '#212121');
+        
+        $scope.auth = auth;
+        
         //grab the auth0 info and then use that to hit the profile endpoint on our api
-      auth.getProfile().then(function(p){
+        auth.getProfile().then(function (p) {
 
-            profilemanagement.getProfile(p.user_id).success(function(d){
-      $scope.profile = d
-      console.log('profile', $scope.profile)
+            profilemanagement.getProfile(p.user_id).success(function (d) {
+                $scope.profile = d
+                console.log('profile', $scope.profile)
 
-    });
+            });
         })
 
-      //update email modal stuff
-       $scope.updateEmail = function () {
+        //update email modal stuff
+        $scope.updateEmail = function () {
 
-         var emailModalInstance = $modal.open({
-           animation: true,
-           templateUrl: 'views/changeEmail.html',
-           controller: 'ChangeemailCtrl',
-           backdropClass: 'modal-background',
-           resolve: {
-             user: function () {
-               return auth.profile;
-             }
-           }
-
-
-         })
-
-         emailModalInstance.result.then(function (result) {
-            auth.profile.email = result
-
-          }, function () {
-
-          });
+            var emailModalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'views/changeEmail.html',
+                controller: 'ChangeemailCtrl',
+                backdropClass: 'modal-background',
+                resolve: {
+                    user: function () {
+                        return auth.profile;
+                    }
+                }
 
 
+            })
 
-       }
+            emailModalInstance.result.then(function (result) {
+                auth.profile.email = result
 
-    // change password modal stuff
-      $scope.changePassword = function () {
-        /*
-         var passwordModalInstance = $modal.open({
-           animation: true,
-           templateUrl: 'views/changePassword.html',
-           controller: 'ChangepasswordCtrl',
-           resolve: {
-             user: function () {
-               return auth.profile;
-             }
-           }
+            }, function () {
 
-
-         })
-
-         passwordModalInstance.result.then(function (result) {
-          auth.profile.email = result
-          }, function () {
-
-          });
-          */
-          auth.reset();
-       }
-
-    //update email preferences modal
-
-        $scope.emailPreferences = function () {
-         var emailPreferenceModalInstance = $modal.open({
-           animation: true,
-           scope: $scope,
-           templateUrl: 'views/emailPreferences.html',
-           controller: 'EmailpreferencesCtrl',
-           resolve: {
-             profile: function () {
-               return $scope.profile;
-             }
-           }
-         })
-
-         emailPreferenceModalInstance.result.then(function (result) {
-             profilemanagement.getProfile(auth.profile.user_id).success(function(d){
             });
 
-                  profilemanagement.getProfile(auth.profile.user_id).success(function(d){
-      $scope.profile = d
-    });
-          }, function () {
-          });
-       }
 
 
-  //update billing modal
+        }
+
+        // change password modal stuff
+        $scope.changePassword = function () {
+            /*
+             var passwordModalInstance = $uibModal.open({
+               animation: true,
+               templateUrl: 'views/changePassword.html',
+               controller: 'ChangepasswordCtrl',
+               resolve: {
+                 user: function () {
+                   return auth.profile;
+                 }
+               }
+    
+    
+             })
+    
+             passwordModalInstance.result.then(function (result) {
+              auth.profile.email = result
+              }, function () {
+    
+              });
+              */
+            auth.reset();
+        }
+
+        //update email preferences modal
+
+        $scope.emailPreferences = function () {
+            var emailPreferenceModalInstance = $uibModal.open({
+                animation: true,
+                scope: $scope,
+                templateUrl: 'views/emailPreferences.html',
+                controller: 'EmailpreferencesCtrl',
+                resolve: {
+                    profile: function () {
+                        return $scope.profile;
+                    }
+                }
+            })
+
+            emailPreferenceModalInstance.result.then(function (result) {
+                profilemanagement.getProfile(auth.profile.user_id).success(function (d) {
+                });
+
+                profilemanagement.getProfile(auth.profile.user_id).success(function (d) {
+                    $scope.profile = d
+                });
+            }, function () {
+            });
+        }
+
+
+        //update billing modal
 
         $scope.updateBilling = function () {
 
-         var updateBillingModalInstance = $modal.open({
-           animation: true,
-           scope: $scope,
-           templateUrl: 'views/updateBillingMethod.html',
-           controller: 'UpdatebillingmethodCtrl',
-           resolve: {
-             profile: function () {
-               return $scope.profile;
-             }
-           }
-         })
+            var updateBillingModalInstance = $uibModal.open({
+                animation: true,
+                scope: $scope,
+                templateUrl: 'views/updateBillingMethod.html',
+                controller: 'UpdatebillingmethodCtrl',
+                resolve: {
+                    profile: function () {
+                        return $scope.profile;
+                    }
+                }
+            })
 
-         updateBillingModalInstance.result.then(function (result) {
-              profilemanagement.getProfile(auth.profile.user_id).success(function(d){
-            $scope.profile = d
+            updateBillingModalInstance.result.then(function (result) {
+                profilemanagement.getProfile(auth.profile.user_id).success(function (d) {
+                    $scope.profile = d
+                });
+
+            }, function () {
+
             });
-
-          }, function () {
-
-          });
-       }
+        }
 
 
         //update billing modal
 
         $scope.cancelMembership = function () {
 
-         var cancelMembershipModalInstance = $modal.open({
-           animation: true,
-           scope: $scope,
-           templateUrl: 'views/cancelMembership.html',
-           controller: 'CancelmembershipCtrl',
-           resolve: {
-             profile: function () {
-               return $scope.profile;
-             }
-           }
+            var cancelMembershipModalInstance = $uibModal.open({
+                animation: true,
+                scope: $scope,
+                templateUrl: 'views/cancelMembership.html',
+                controller: 'CancelmembershipCtrl',
+                resolve: {
+                    profile: function () {
+                        return $scope.profile;
+                    }
+                }
 
-         })
+            })
 
-         cancelMembershipModalInstance.result.then(function (result) {
-              profilemanagement.getProfile(auth.profile.user_id).success(function(d){
-            $scope.profile = d
+            cancelMembershipModalInstance.result.then(function (result) {
+                profilemanagement.getProfile(auth.profile.user_id).success(function (d) {
+                    $scope.profile = d
 
+                });
+                $scope.profile.billing_method = 'no billing method/membership canceled';
+            }, function () {
             });
-             $scope.profile.billing_method = 'no billing method/membership canceled';
-          }, function () {
-          });
-       }
+        }
 
 
-      $scope.billingHistory = function(){
-          var billingHistoryModalInstance = $modal.open({
-           animation: true,
-           scope: $scope,
-           templateUrl: 'views/billingHistory.html',
-           controller: 'BillinghistoryCtrl',
-           resolve: {
-             profile: function () {
-               return $scope.profile;
-             }
-           }
-         })
-      }
-  });
+        $scope.billingHistory = function () {
+            var billingHistoryModalInstance = $uibModal.open({
+                animation: true,
+                scope: $scope,
+                templateUrl: 'views/billingHistory.html',
+                controller: 'BillinghistoryCtrl',
+                resolve: {
+                    profile: function () {
+                        return $scope.profile;
+                    }
+                }
+            })
+        }
+    });
