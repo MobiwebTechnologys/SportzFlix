3aa7b861e6a8877f4fc7183500a491d22bdcce7d
ashishg142 <ashishg@mobiwebtech.com>
2016-02-12 18:26:28 +0530

    slider page changes

--- app/scripts/directives/menu.js
+++ app/scripts/directives/menu.js
@@ -45,3 +45,17 @@ angular.module('sportzflixApp')
 
         }
     })
+	
+	/*Mobiweb Technologys Pvt. Ltd.*/
+
+   .directive('errSrc', function () { // image 404 status checking
+        return {
+            link: function (scope, element, attrs) {
+                element.bind('error', function () {
+                    if (attrs.src != attrs.errSrc) {
+                        attrs.$set('src', attrs.errSrc);
+                    }
+                });
+            }
+        };
+    });
